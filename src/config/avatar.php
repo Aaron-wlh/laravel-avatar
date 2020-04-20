fb48e6d75d01ba79fc2426bea076552cceeaa9a4
Aaron_wlh <976724250@qq.com>
2020-04-20 05:46:35 +0000

    init

---
+++ src/config/avatar.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/4/20/020
+ * Time: 13:23
+ */
+
+return   [
+    'type' => 'png', // jpeg|png|gif|bmp
+    'width' => '100',
+    'height' => '100',
+    'size' => '26',
+    'font_file' => public_path() . '/fonts/WawaSC-Regular.otf',
+];
